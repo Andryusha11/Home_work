93889bf02db4b53c0b4f9d421cdba740087b1e3d
andrew_valchuk <andrewv2502@gmail.com>
2021-11-19 11:10:27 +0200

    add all project

---
+++ app.js
@@ -0,0 +1,15 @@
+const sliders = document.querySelectorAll('.slider');
+
+for (const slider of sliders) {
+	slider.addEventListener('click', () => {
+		clearActiveClasses()
+
+		slider.classList.add('active')
+	})
+};
+
+function clearActiveClasses() {
+	sliders.forEach((slider) => {
+		slider.classList.remove('active')
+	})
+}
